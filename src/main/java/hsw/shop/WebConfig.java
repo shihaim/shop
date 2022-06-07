a4142cc9d5882b43d5e818178ced134ce1bf7db3
shihaim <hashi00518@gmail.com>
2022-06-07 16:10:49 +0900

    LoginCheckInterceptor, WebConfig 추가

---
+++ src/main/java/hsw/shop/WebConfig.java
@@ -0,0 +1,17 @@
+package hsw.shop;
+
+import hsw.shop.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/sign-up", "/sign-in", "/logout", "/css/**", "/*.ico", "/error");
+    }
+}
