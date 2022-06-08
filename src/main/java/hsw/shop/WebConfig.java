643ddea5f5fafed43dd372baab234cf0c63d1eca
shihaim <hashi00518@gmail.com>
2022-06-08 19:48:19 +0900

    로그인 세션용 ArgumentResolver 추가

--- src/main/java/hsw/shop/WebConfig.java
+++ src/main/java/hsw/shop/WebConfig.java
@@ -1,17 +1,28 @@
 package hsw.shop;
 
+import hsw.shop.web.argumentresolver.LoginMemberArgumentResolver;
 import hsw.shop.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    /*@Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/sign-up", "/sign-in", "/logout", "/css/**", "/*.ico", "/error");
-    }
+        //ADMIN일 경우 상품 등록 가능
+    }*/
 }
